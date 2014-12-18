d454c673648eea3446afdb76fa77644c5bb18842
J. Harshbarger <hypercubed@gmail.com>
2014-12-18 14:46:52 +0900

    Improve

--- app/components/app.js
+++ app/components/app.js
@@ -21,17 +21,20 @@ angular
   ])
   .config(function ($routeProvider) {
 
+    var resolve = {
+      index: function($route, DataService) { return DataService.load($route.current.params.id); }
+    };
+
     $routeProvider
+      .when('/preview/:id/', {
+        templateUrl: 'components/main/preview.html',
+        controller: 'MainCtrl as main',
+        resolve: resolve
+      })
       .when('/:id/', {
         templateUrl: 'components/main/main.html',
         controller: 'MainCtrl as main',
-        resolve: {
-          index: function($route, DataService) { return DataService.load($route.current.params.id); }
-        }
-      })
-      .when('/about', {
-        templateUrl: 'components/about/about.html',
-        controller: 'AboutCtrl'
+        resolve: resolve
       })
       .otherwise({
         redirectTo: '/index'
