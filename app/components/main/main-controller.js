d865d8710648765320d1a698e64e1f74ad743ca7
J. Harshbarger <hypercubed@gmail.com>
2014-12-24 13:41:47 +0900

    WIP gist

--- app/components/main/main-controller.js
+++ app/components/main/main-controller.js
@@ -1,4 +1,5 @@
 /* global Papa:true */
+/* global $:true */
 
 'use strict';
 
@@ -18,36 +19,77 @@ angular.module('myApp')
       for (var key in files) {delete files[key];}
     }
 
-    function loadFile(file) {
+    var types = {
+      'text/html':                  ['html','htm'],
+      'text/tab-separated-values':  ['tsv'],
+      'application/json':           ['json'],
+      'text/css':                   ['css'],
+      'text/csv':                   ['csv'],
+      'application/javascript':     ['js']
+    };
 
-      if (!angular.isObject(file)) {
-        file = { name: file, show: true };
+    function getType(ext) {
+
+      for (var type in types) {
+        if (types[type].indexOf(ext) > -1) {
+          return type;
+        }
       }
 
-      angular.extend(file, {
-        path: file.name.split('/')[0]
-      });
+      return 'unknown';
 
-      $log.debug('Loading ',file.name, file);
+    }
 
-      var transform = function(data) {
-        file.content = data;
-        process(file);
-        return file;
-      };
+    function loadFile(file) {
 
-      return $http.get('data/'+file.name, {transformResponse: transform})
+      file.url = file.url || file.filename;
+
+      var _path = file.url.split('/');
+      _path.shift();
+      var _ext = _path[_path.length -1].split('.').pop();
+
+      file = angular.extend({
+        name: _path.join('/'),
+        //id: _path[0],
+        path: _path[0],
+        filename: _path[1],
+        ext: _ext,
+        type: getType(_ext),
+      }, file);
+
+      if (file.type.indexOf('image') === 0) {
+        file.show = false;
+      }
+
+      $log.debug('Loading ',file.url);
+
+      if (!file.content) {
+
+        var transform = function(data) {
+          file.content = data;
+          process(file);
+          return file;
+        };
+
+        return $http.get(file.url, {transformResponse: transform})
         .success(function(_file) {
           files[_file.name] = _file;
+          //console.log(_file);
         });
+
+      } else {
+        files[file.name] = file;
+        //console.log(file);
+        return file;
+      }
+
     }
 
     function process(file) {
-      var ext = file.name.split('.').pop();
-      if (['tsv'].indexOf(ext) > -1) {
+      if (file.ext === 'tsv') {  // TODO: more file types
         var parse = Papa.parse(file.content, {header: true, delimiter: '\t', skipEmptyLines: true});
         angular.extend(file, parse);
-      } else if (ext === 'json') {
+      } else if (file.ext === 'json') {
         file.data = angular.fromJson(file.content);
       }
     }
@@ -60,22 +102,39 @@ angular.module('myApp')
 
       clear();
 
-      return loadFile({name: id+'/index.json', show: false})
+      return loadFile({url: 'data/'+id+'/index.json', show: false})
         .then(function(res) {
-          var files = res.data.data.files || [id+'/index.html'];
+          var files = res.data.data.files || ['data/'+id+'/index.html'];
+          var q = [];
 
-          var q = files.map(function(file) {
+          if (angular.isObject(files) && !angular.isArray(files)) {
 
-            if (!angular.isObject(file)) {
-              file = { name: file, show: true };
-            }
+            angular.forEach(files, function(file) {
+              file.url = file.url || file.filename;
 
-            if (file.name.indexOf('/') < 0) {
-              file.name = id+'/'+file.name;
-            }
 
-            return loadFile(file);
-          });
+              if (file.url.indexOf('/') < 0) {
+                file.url = 'data/'+id+'/'+file.url;
+              }
+
+              q.push(loadFile(file));
+
+            });
+
+          } else {
+            q = files.map(function(file) {
+
+              if (!angular.isObject(file)) {
+                file = { url: file, show: true };
+              }
+
+              if (file.url.indexOf('/') < 0) {
+                file.url = 'data/'+id+'/'+file.url;
+              }
+
+              return loadFile(file);
+            });
+          }
 
           return $q.all(q);
         });
@@ -173,7 +232,7 @@ angular.module('myApp')
 
 })
 
-.directive('reportInclude', function($window, $compile, $timeout, $log) {
+.directive('reportInclude', function($window, $compile, $timeout, $log, $routeParams) {
   return {
     restrict: 'AE',
     link: function(scope, element, attr) {
@@ -184,6 +243,17 @@ angular.module('myApp')
       var reportScope = scope.$new();
       $window.files = reportScope.files = files;
 
+      var d3_tsv = d3.tsv;
+
+      d3.tsv = function(filename, callback) { // TODO: move
+        var file = files[$routeParams.id + '/' + filename];
+        if (file && file.data) {
+          return callback(null, file.data);
+        }
+        return d3_tsv(filename, callback);
+      }
+
+
       function handleFiles() {
 
         $log.debug('Generating report');
@@ -192,17 +262,23 @@ angular.module('myApp')
 
         reportContainer = angular.element('<div class="report-content">');
 
-        angular.forEach(files, function(file) { // TODO: handle, CSS, JS files
-          if (file.name.indexOf('index.html') > 0) {
+        var content = '';
 
-            //reportScope.$evalAsync(function() {  // Do this last
-              element.append(reportContainer);
-              reportContainer.html(file.content);
-              $compile(reportContainer.contents())(reportScope);
-            //});
-
+        angular.forEach(files, function(file) {  // TODO: improve this, place in correct place, in correct order, handle other types
+          if (file.filename === 'index.html' || file.filename === 'index.htm') {
+            content += file.content;
+          } else if (file.type === 'text/css') {
+            content += '<style>'+file.content+'</style>';
+          } else if (file.type === 'application/javascript') {
+            content += '<script>'+file.content+'</script>';
           }
         });
+
+        //reportScope.$evalAsync(function() {  // Do this last
+          element.append(reportContainer);
+          reportContainer.html(content);
+          $compile(reportContainer.contents())(reportScope);
+        //});
 
         $log.debug('Generating report - done');
 
